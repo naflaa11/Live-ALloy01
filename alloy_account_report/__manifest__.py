e62dbf70805ea7ce5fb6a6ce4f1afdeaa3043925
naflaa11 <53143354+naflaa11@users.noreply.github.com>
2020-01-09 13:25:10 +0200

    Add files via upload

---
+++ alloy_account_report/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+# 
+#
+##############################################################################
+{
+    'name': 'Alloy Account Report',
+    'description': """
+     
+    """,
+    'version': '1.0',
+    'category': 'Account',
+    'depends': ['account_reports'],
+    'data': [        
+        # 'report/follow.xml',
+    ],
+    'demo': [],
+    'auto_install': False,
+    'application': False,
+    'installable': True
+}
